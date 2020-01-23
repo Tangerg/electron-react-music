0f1c3a1c3da8a1e89f7583a851747687e0bed181
Tangerg <tangerg@qq.com>
2020-01-23 19:23:55 +0800

    react结合ts初探，安装部分依赖

--- src/renderer/layout/BasicLayout.tsx
+++ src/renderer/layout/BasicLayout.tsx
@@ -1,16 +1,58 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 import {Layout} from 'antd';
+import './BasicLayout.less'
 
 const {Header, Footer, Sider, Content} = Layout;
 
-class BasicLayout extends React.Component<any, any> {
+interface State {
+    isMove: Boolean
+}
+
+class BasicLayout extends React.Component<any, State> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isMove: false
+        }
+    }
+
+    mouseDown = () => {
+        const myComp = this.refs.myInput;
+        const myComp1 = ReactDom.findDOMNode(myComp);
+        console.log(myComp);
+        console.log(myComp1);
+        console.log(this.state.isMove);
+        this.setState({
+            isMove: true
+        });
+        console.log(this.state.isMove);
+        console.log('mouseDown')
+    };
+    mouseUp = () => {
+        const myComp = this.refs.myInput;
+        const myComp1 = ReactDom.findDOMNode(myComp);
+        console.log(myComp);
+        console.log(myComp1);
+        console.log(this.state.isMove);
+        this.setState({
+            isMove: false
+        });
+        console.log('mouseUp')
+    };
+
     render() {
         return (
             <div>
-                <Layout>
+                <Layout className={'basic-layout'}>
                     <Sider>Sider</Sider>
                     <Layout>
-                        <Header>Header</Header>
+                        <Header
+                            onMouseDown={this.mouseDown}
+                            onMouseUp={this.mouseUp}
+                            ref='myInput'
+                            className={["basic-layout-header", this.state.isMove ? "can-move" : null].join(' ')}>
+                            头部</Header>
                         <Content>Content</Content>
                         <Footer>Footer</Footer>
                     </Layout>
